f1fee7cec3706e91c0c8aea5e7de10ee0d3fab1e
David Gasinski <dgasinski1@sheffield.ac.uk>
2023-11-11 15:15:59 +0000

    added prettier and lint

--- app/page.js
+++ app/page.js
@@ -1,38 +1,38 @@
-'use client'
+'use client';
 
 import { useEffect, useState } from 'react';
 
-import Image from 'next/image'
+import Image from 'next/image';
 import Pusher from 'pusher-js';
 
 export default function Home() {
-  const [clientId, setClientId] = useState(0)
-  const [username, setUsername] = useState('Kush')
-  const [chats, setChats] = useState([])
+  const [clientId, setClientId] = useState(0);
+  const [username, setUsername] = useState('Kush');
+  const [chats, setChats] = useState([]);
 
   function updateChats(data) {
-    console.log(data)
-    setChats([...chats, data])
+    console.log(data);
+    setChats([...chats, data]);
   }
 
   useEffect(() => {
     const pusher = new Pusher(process.env.NEXT_PUBLIC_PUSHER_KEY, {
       cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER,
-      encrypted: true
-    })
+      encrypted: true,
+    });
 
-    const channel = pusher.subscribe('test-uno')
-    console.info('Connected to test-uno')
+    const channel = pusher.subscribe('test-uno');
+    console.info('Connected to test-uno');
 
-    channel.bind('init', data => {
-      updateChats(data)
-      channel.trigger('')
+    channel.bind('init', (data) => {
+      updateChats(data);
+      channel.trigger('');
     });
 
     return () => {
       // pusher.disconnect();
-    }
-  })
+    };
+  });
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -41,5 +41,5 @@ export default function Home() {
         <p>{element}</p>
       })} */}
     </main>
-  )
+  );
 }
