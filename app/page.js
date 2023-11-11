2fbd9a31b1b5df9a2df2abff5a89d013b8e180c3
Kush Makkapati <kush.makkapati@icloud.com>
2023-11-11 15:05:27 +0000

    Add initial socketio connection

--- app/page.js
+++ app/page.js
@@ -1,42 +1,75 @@
 'use client'
 
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react'
 
-import Image from 'next/image'
-import Pusher from 'pusher-js';
+import { io } from 'socket.io-client'
+
+const URL = 'http://143.167.68.112:696/'
 
 export default function Home() {
-  const [clientId, setClientId] = useState(0)
+  const [socket, setSocket] = useState()
+  const [isConnected, setIsConnected] = useState(false)
+  const [clientId, setClientId] = useState()
   const [username, setUsername] = useState('Kush')
-  const [chats, setChats] = useState([])
 
-  function updateChats(data) {
-    console.log(data)
-    setChats([...chats, data])
+  function connectToServer(e) {
+    e.preventDefault()
+    if (!socket) {
+      console.log('Socket not instantiated yet')
+      return
+    }
+
+    console.log('Connecting to socket')
+    socket.connect()
   }
 
   useEffect(() => {
-    const pusher = new Pusher(process.env.NEXT_PUBLIC_PUSHER_KEY, {
-      cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER,
-      encrypted: true
-    })
+    console.log('Initalising socket')
+    const socket = io(URL, { autoConnect: false })
+    console.log('Socket successfully initialised')
+    setSocket(socket)
 
-    const channel = pusher.subscribe('test-uno')
-    console.info('Connected to test-uno')
+    function onConnect(socket) {
+      setIsConnected(true);
+      console.log(`Connected to server, client id: ${socket.id}`)
+      setClientId(socket.id)
+    }
 
-    channel.bind('init', data => {
-      updateChats(data)
-      channel.trigger('')
-    });
+    function onDisconnect() {
+      console.log('Disconnected from server')
+      setIsConnected(false);
+      setClientId(null)
+    }
+
+    function onFooEvent(value) {
+      setFooEvents(previous => [...previous, value]);
+    }
+
+    socket.on('connect', onConnect);
+    socket.on('disconnect', onDisconnect);
+    socket.on('foo', onFooEvent);
 
     return () => {
-      // pusher.disconnect();
-    }
-  })
+      socket.off('connect', onConnect);
+      socket.off('disconnect', onDisconnect);
+      socket.off('foo', onFooEvent);
+      socket.disconnect();
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Username: {username}</h1>
+      <form onSubmit={connectToServer}>
+        <label>Enter your name:
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </label>
+        <input type="submit" />
+      </form>
       {/* {chats.forEach(element => {
         <p>{element}</p>
       })} */}
