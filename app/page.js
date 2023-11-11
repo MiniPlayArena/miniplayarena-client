429145b8564a501e6934a09500359b5dc5167d03
Kush Makkapati <kush.makkapati@icloud.com>
2023-11-11 15:30:47 +0000

    Update socket connection code

--- app/page.js
+++ app/page.js
@@ -7,29 +7,16 @@ import { io } from 'socket.io-client';
 const URL = 'http://143.167.68.112:696/';
 
 export default function Home() {
-  const [socket, setSocket] = useState();
   const [isConnected, setIsConnected] = useState(false);
   const [clientId, setClientId] = useState();
   const [username, setUsername] = useState('Kush');
 
-  function connectToServer(e) {
-    e.preventDefault();
-    if (!socket) {
-      console.log('Socket not instantiated yet');
-      return;
-    }
-
-    console.log('Connecting to socket');
-    socket.connect();
-  }
 
   useEffect(() => {
-    console.log('Initalising socket');
-    const socket = io(URL, { autoConnect: false });
-    console.log('Socket successfully initialised');
-    setSocket(socket);
+    const socket = io(URL);
+    console.log('Socket successfully connected');
 
-    function onConnect(socket) {
+    function onConnect() {
       setIsConnected(true);
       console.log(`Connected to server, client id: ${socket.id}`);
       setClientId(socket.id);
@@ -60,7 +47,7 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <form onSubmit={connectToServer}>
+      <form>
         <label>
           Enter your name:
           <input
