c2024ea0bae94a15a809d1f1b89f19155ba20486
David Gasinski <dgasinski1@sheffield.ac.uk>
2023-11-11 15:21:13 +0000

    added prettier and json

--- app/page.js
+++ app/page.js
@@ -2,39 +2,6 @@
 
 import { useEffect, useState } from 'react'
 
-<<<<<<< HEAD
-import Image from 'next/image';
-import Pusher from 'pusher-js';
-
-export default function Home() {
-  const [clientId, setClientId] = useState(0);
-  const [username, setUsername] = useState('Kush');
-  const [chats, setChats] = useState([]);
-
-  function updateChats(data) {
-    console.log(data);
-    setChats([...chats, data]);
-  }
-
-  useEffect(() => {
-    const pusher = new Pusher(process.env.NEXT_PUBLIC_PUSHER_KEY, {
-      cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER,
-      encrypted: true,
-    });
-
-    const channel = pusher.subscribe('test-uno');
-    console.info('Connected to test-uno');
-
-    channel.bind('init', (data) => {
-      updateChats(data);
-      channel.trigger('');
-    });
-
-    return () => {
-      // pusher.disconnect();
-    };
-  });
-=======
 import { io } from 'socket.io-client'
 
 const URL = 'http://143.167.68.112:696/'
@@ -90,7 +57,6 @@ export default function Home() {
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
->>>>>>> 24b8c475590935d9cf778a506099678663699312
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
