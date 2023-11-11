65f54090429ccb41b9a4680979f0c19fec7cbac4
Kush Makkapati <kush.makkapati@icloud.com>
2023-11-11 16:29:30 +0000

    Add party creation

--- app/page.js
+++ app/page.js
@@ -1,17 +1,21 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import { Index } from '.';
 
+import { CLIENT_STATIC_FILES_RUNTIME_REACT_REFRESH } from 'next/dist/shared/lib/constants';
+import { Index } from '.';
 import { io } from 'socket.io-client';
 
 const URL = 'http://143.167.68.112:696/';
 
 export default function Home() {
-  const [socket, setSocket] = useState(null);
-  const [isConnected, setIsConnected] = useState(false);
-  const [clientId, setClientId] = useState();
-  const [username, setUsername] = useState('Kush');
+  const [socket, setSocket] = useState(null)
+  const [isConnected, setIsConnected] = useState(false)
+  const [clientId, setClientId] = useState()
+  const [username, setUsername] = useState('Kush')
+  const [usernameAdded, setUsernameAdded] = useState(false)
+  const [party, setParty] = useState(null)
+
   function emitUsernameToServer(e) {
     e.preventDefault();
     if (!socket) {
@@ -22,7 +26,7 @@ export default function Home() {
     socket.emit('create-player', { clientId: clientId, username: username });
   }
 
-  function emitCreateParty(e) {
+  function emitCreateParty() {
     if (!socket) {
       console.error('Socket not connected');
       return;
@@ -49,20 +53,41 @@ export default function Home() {
       console.log('Disconnected from server');
       setIsConnected(false);
       setClientId(null);
+      setUsernameAdded(false);
+      setParty(null);
     }
 
     function onError(data) {
-      console.error(data);
+      console.error(data.message);
+    }
+
+    function onPlayerCreated(data) {
+      if (data.clientId) {
+        setUsernameAdded(true);
+      }
+    }
+
+    function onPartyCreated(data) {
+      console.log(data)
+      setParty((prevState) => ({
+        ...prevState,
+        partyId: data.partyId,
+        partyLeader: data.partyLeader
+      }));
     }
 
     _socket.on('connect', onConnect);
     _socket.on('disconnect', onDisconnect);
     _socket.on('error', onError);
+    _socket.on('player-created', onPlayerCreated)
+    _socket.on('party-created', onPartyCreated)
 
     return () => {
       _socket.off('connect', onConnect);
       _socket.off('disconnect', onDisconnect);
       _socket.off('error', onError);
+      _socket.off('player-created', onPlayerCreated)
+      _socket.off('party-created', onPartyCreated)
       _socket.disconnect();
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -71,21 +96,25 @@ export default function Home() {
   return (
     <Index>
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <form onSubmit={emitUsernameToServer}>
-          <label>
-            Enter your name:
-            <input
-              className="text-black"
-              type="text"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-          </label>
-          <input type="submit" />
-        </form>
-        {/* {chats.forEach(element => {
-        <p>{element}</p>
-      })} */}
+        {!usernameAdded ? (
+          <form onSubmit={emitUsernameToServer}>
+            <label>
+              Enter your name:
+              <input
+                className="text-black"
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </label>
+            <input type="submit" />
+          </form>
+        ) : (
+          <>
+            <p>{username}</p>
+            {party !== null ? (<p>Party ID: {party.partyId}</p>) : (<button onClick={emitCreateParty}>Create party</button>)}
+          </>
+        )}
       </main>
     </Index>
   );
