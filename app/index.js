9c11c3cd220d5387c7778119d937f8346956ed47
David Gasinski <dgasinski1@sheffield.ac.uk>
2023-11-11 17:23:55 +0000

    rendering children prop

--- app/index.js
+++ app/index.js
@@ -1,22 +1,15 @@
 /** HOME */
 'use client'
-
-import { Button, Container } from '@chakra-ui/react'
-
+import { Container, Button } from '@chakra-ui/react'
 import { StyledButton } from './components/themes/button'
 
 const backgroundUrl = '' //https://i.pinimg.com/originals/6f/c3/07/6fc307f1950ce15f220aeee764e219e5.gif'
 
 export function Index({ children }) {
   return (
-    <Container
-      minH={'calc(100vh)'}
-      minW={'calc(100vw)'}
-      bg='salmon.100'>
-      <StyledButton>
-        Hello World!
-        {children}
-      </StyledButton>
+     <Container minH={'calc(100vh)'} minW={'calc(100vw)'} bg="salmon.100">
+      <StyledButton>Hello World!</StyledButton>
+      {children}
     </Container>
-  );
+  )
 }
